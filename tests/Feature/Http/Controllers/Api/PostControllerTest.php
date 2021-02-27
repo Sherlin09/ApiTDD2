ae755748883addbf4bf46a08e2ee8f8bb7a4ccf0
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-02-27 01:27:54 -0600

    5_4

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -6,21 +6,26 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Post;
+use App\User;
+
 class PostControllerTest extends TestCase
 {
-    
+    use RefreshDatabase;
+
     public function test_store()
     {
         //$this ->withoutExceptionHandling();
         $user = factory(User::class)->create();
+        //$this->withoutExceptionHandling();
         $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
             'title' => 'El post de prueba'
         ]);
 
         $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
-                ->assertJson(['title' => 'El post de prueba'])
-                ->assertStatus(201); //OK, creado un recurso
+            ->assertJson(['title'=> 'El post de prueba'])
+            ->assertStatus(201);
 
-                $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
     }
 }
