a1783a8f484a72d4f871f521c11991ff8a04dfd6
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-02-27 01:11:21 -0600

    test-store() completo

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,19 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    
+    public function test_store()
     {
-        $response = $this->get('/');
+        //$this ->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => 'El post de prueba'
+        ]);
 
-        $response->assertStatus(200);
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+                ->assertJson(['title' => 'El post de prueba'])
+                ->assertStatus(201); //OK, creado un recurso
+
+                $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
     }
 }
